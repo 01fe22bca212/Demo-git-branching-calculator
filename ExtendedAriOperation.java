6b0fc420c55152b7252b9d40d4b11955bda5fb34
vaishnavi kala <01fe22bca212@kletech.ac.in>
2024-02-14 16:12:45 +0530

    updated branch code

---
+++ ExtendedAriOperation.java
@@ -0,0 +1,23 @@
+    class ExtendedAriOperation{
+    public float square(float num1, float num2)
+        {
+            //float pow = 2;
+           float squ=num1*num1;
+            return squ;
+        }
+        public float  cube(float num1, float num2)
+        {
+            float cube=num1*num1*num1;
+            return cube;
+        }
+        public float  Modules(float num1, float num2)
+        {
+            float modules=num1%num2;
+            return modules;
+        }
+        public float areaoftriangle(float num1, float num2)
+        {
+            float area=0.5*a*b;
+            return area;
+        }
+    }
